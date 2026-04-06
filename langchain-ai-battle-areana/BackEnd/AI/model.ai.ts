d1d1467a0d93ad5bb5fbd404fb25f847fe23056d
abhay bawne <abhaybawne070@gmail.com>
2026-04-06 09:45:33 +0530

    feat: add models for Gemini, MistralAI, and Cohere with API key integration

--- langchain-ai-battle-areana/BackEnd/AI/model.ai.ts
+++ langchain-ai-battle-areana/BackEnd/AI/model.ai.ts
@@ -1,7 +1,21 @@
 import {ChatGoogle} from "@langchain/google"
 import { ChatCohere } from "@langchain/cohere"
 import { ChatMistralAI } from "@langchain/mistralai"
+import config from "../src/config/config.js"
 
 
 
+export const geminiModel  = new ChatGoogle({
+    model:"gemini-flash-latest",
+    apiKey:config.GOOGLE_API_KEY,
+})
 
+export const mistralAIModel = new ChatMistralAI({
+    model:"mistal-meduim-lates",
+    apiKey:config.Mistral_API_KEY,
+})
+
+export const cohereModel = new ChatCohere({
+    model:"cohere-command-a-2025",
+    apiKey:config.COHERE_API_KEY
+})
