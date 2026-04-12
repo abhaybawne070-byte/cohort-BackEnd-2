1fa30a50878330cfe19ac1b7ca8e8378d3357be1
abhay bawne <abhaybawne070@gmail.com>
2026-04-12 23:41:47 +0530

    feat: set up server with express, add user model, and implement authentication routes

--- snitch-product/BackEnd/src/app.js
+++ snitch-product/BackEnd/src/app.js
@@ -1,7 +1,7 @@
 import express from  'express'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
-
+import authRouter from "./routes/auth.routes.js"
 
 const app=express()
 
@@ -14,5 +14,7 @@ app.use(cookieParser())
 app.get("/", (req,res) => {
     res.status(200).json({message:"server is running"})
 })
+
+app.use("/api/auth",authRouter)
 
 export default app;
