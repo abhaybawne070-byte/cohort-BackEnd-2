235ca900f823fd4788f5503fd539a4b042bbd916
abhay bawne <abhaybawne070@gmail.com>
2026-04-13 22:26:57 +0530

    Add has-tostringtag and proxy-from-env packages with necessary files
    - Introduced `has-tostringtag` package to determine if the JS environment has `Symbol.toStringTag` support, including:
      - LICENSE, README.md, index.js, index.d.ts, package.json, and test files.
    
    - Added `proxy-from-env` package to handle proxy URLs based on environment variables, including:
      - LICENSE, README.md, index.cjs, index.js, and package.json.
    
    - Created initial auth API service and Redux slice for authentication in the frontend:
      - Added `auth.api.js` for Axios instance configuration.
      - Added `auth.slice.js` for managing authentication state with Redux Toolkit.

--- snitch-product/Frontend/vite-project/src/app/app.store.js
+++ snitch-product/Frontend/vite-project/src/app/app.store.js
@@ -1,4 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit"
+import authReducer from "../feature/auth/state/auth.slice"
+
 
 export const store = configureStore({
     reducer:{
---
+++ snitch-product/Frontend/vite-project/src/feature/auth/service/auth.api.js
@@ -0,0 +1,7 @@
+import axios from "axios"
+
+
+
+const authApiInstance = axios.create({
+    baseURL:""
+})
